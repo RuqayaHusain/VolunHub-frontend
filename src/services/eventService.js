81434168f993b44b6a827584ef1cedaa5129c94f
Ruqaya Husain <ruqayanader22@gmail.com>
2025-11-14 16:35:18 +0300

    added createEvent function to eventService

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,1 +1,23 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/events`;
+
+const createEvent = async (eventFormData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: { 
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(eventFormData)
+        });
+        return res.json();
+    } catch (error) {
+       console.log(error); 
+    }
+};
+
+
+
+export {
+    createEvent,
+}
