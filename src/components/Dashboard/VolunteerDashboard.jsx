619fbcf5424847c32a07652b149dcae33fb0f28f
alialgallaf <alialgallaf06@gmail.com>
2025-11-16 01:49:35 +0300

    Fetch and display volunteer applications

---
+++ src/components/Dashboard/VolunteerDashboard.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import * as dashboardService from "../../services/dashboardService.js";
+import DashboardCard from './DashboardCard.jsx';
+
+const VolunteerDashboard = () => {
+  const { user } = useContext(UserContext);
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    const fetchApplications = async () => {
+      const data = await dashboardService.getVolunteerApplications();
+      setApplications(data.applications || []);
+    };
+
+    if (user) fetchApplications();
+  }, [user]);
+
+  const uniqueEvents = Array.from(
+    applications.reduce((map, app) => {
+      if (app.event && !map.has(app.event._id)) {
+        map.set(app.event._id, app);
+      }
+      return map;
+    }, new Map()).values()
+  );
+
+  return (
+    <main>
+      <h1>My Applications</h1>
+
+      {uniqueEvents.length === 0 ? (
+        <p>You haven’t applied to any events yet.</p>
+      ) : (
+        <div>
+          {uniqueEvents.map((app) => (
+            <DashboardCard key={app._id} application={app} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+};
+
+
+export default VolunteerDashboard;
