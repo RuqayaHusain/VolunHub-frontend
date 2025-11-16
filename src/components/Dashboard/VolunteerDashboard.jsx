c81181936f2f008530bbce00f78882ca7f5bd519
alialgallaf <alialgallaf06@gmail.com>
2025-11-16 19:35:50 +0300

    fix dashboard cards

--- src/components/Dashboard/VolunteerDashboard.jsx
+++ src/components/Dashboard/VolunteerDashboard.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import * as dashboardService from "../../services/dashboardService.js";
@@ -17,25 +16,16 @@ const VolunteerDashboard = () => {
     if (user) fetchApplications();
   }, [user]);
 
-  const uniqueEvents = Array.from(
-    applications.reduce((map, app) => {
-      if (app.event && !map.has(app.event._id)) {
-        map.set(app.event._id, app);
-      }
-      return map;
-    }, new Map()).values()
-  );
-
   return (
     <main>
       <h1>My Applications</h1>
 
-      {uniqueEvents.length === 0 ? (
+      {applications.length === 0 ? (
         <p>You haven’t applied to any events yet.</p>
       ) : (
         <div>
-          {uniqueEvents.map((app) => (
-            <DashboardCard key={app._id} applications={app} />
+          {applications.map((app) => (
+            <DashboardCard key={app._id} application={app} />
           ))}
         </div>
       )}
@@ -43,6 +33,4 @@ const VolunteerDashboard = () => {
   );
 };
 
-
 export default VolunteerDashboard;
-
