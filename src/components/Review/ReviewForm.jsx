84ff35b6e78e9cd80df65cbc0a7f1815a2983978
Manar's <manarmohd15@gmail.com>
2025-11-19 11:59:45 +0300

    starting Review page

---
+++ src/components/Review/ReviewForm.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect, useContext } from 'react';
+import { Star } from 'lucide-react';
+import { UserContext } from '../../contexts/UserContext';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export default ReviewsPage;
